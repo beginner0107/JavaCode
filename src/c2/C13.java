f9dd55b9783841623d84848fff8763d64acd083d
Beginner <dkstmdwn17@naver.com>
2022-04-05 01:15:19 +0900

    큰 수 출력하기
    큰 수 출력하기

--- src/c2/C13.java
+++ src/c2/C13.java
@@ -1,30 +1,36 @@
 package c2;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class C13 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int[]a = new int[n];
-		for(int i=0;i<n;i++) {
+		int[] a = new int[n];
+		for (int i = 0; i < n; i++) {
 			a[i] = sc.nextInt();
 		}
-		
+
 		bigNum(n, a);
 		sc.close();
 	}
 
+	public ArrayList<Integer> solution(int n, int[] arr) {
+		ArrayList<Integer> answer = new ArrayList<Integer>();
+		answer.add(arr[0]);
+		for (int i = 1; i < n; i++) {
+			if (arr[i] > arr[i - 1])
+				answer.add(arr[i]);
+		}
+		return answer;
+	}
+
 	private static void bigNum(int n, int[] a) {
-		System.out.printf("%d ",a[0]);
-		for(int i=1;i<a.length;i++) {
-			if(a[i-1]<a[i]) {
-				if(i==a.length-1) {
-					System.out.print(a[i]);
-				}else {
-					System.out.print(a[i] + " ");
-				}
-			}
+		System.out.printf("%d ", a[0]);
+		for (int i = 1; i < a.length; i++) {
+			if (a[i] > a[i - 1])
+				System.out.print(a[i] + " ");
 		}
 	}
 }
