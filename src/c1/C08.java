f3b69548d93d64b1cafde55f3be8a7d0afdabdd0
Beginner <dkstmdwn17@naver.com>
2022-04-05 01:14:15 +0900

    유효한 펠린드롬
    유효한 펠린드롬

--- src/c1/C08.java
+++ src/c1/C08.java
@@ -18,6 +18,13 @@ public class C08 {
 		}
 		return "YES";
 	}
+	public String solution(String s) {
+		String answer = "NO";
+		s = s.toUpperCase().replaceAll("[^a-zA-Z]", "");
+		String tmp = new StringBuilder(s).reverse().toString();
+		if(s.equals(tmp))answer = "YES";
+		return answer;
+	}
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
