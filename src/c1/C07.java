ba47d5e709d934e30400749bd80bf0d144f460d3
Beginner <dkstmdwn17@naver.com>
2022-04-05 01:14:01 +0900

    회문 문자열
    회문 문자열

--- src/c1/C07.java
+++ src/c1/C07.java
@@ -14,6 +14,22 @@ public class C07 {
 		}
 		System.out.println(answer);
 	}
+	public String solution(String str) {
+		String answer = "YES";
+		str = str.toUpperCase();
+		int len = str.length();
+		for(int i=0;i<len/2;i++) {
+			if(str.charAt(i)!=str.charAt(len-i-1)) return "NO";
+		}
+		return answer;
+	}
+	
+	public String solution1(String str) {
+		String answer = "NO";
+		String tmp = new StringBuilder(str).reverse().toString();
+		if(str.equalsIgnoreCase(tmp)) answer = "YES";
+		return answer;
+	}
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
