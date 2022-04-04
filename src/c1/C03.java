bbc0473bb56e91c8a8a021461f6b713bfd750b3e
Beginner <dkstmdwn17@naver.com>
2022-04-05 01:13:19 +0900

    문장 속 단어
    문장 속 단어

--- src/c1/C03.java
+++ src/c1/C03.java
@@ -16,6 +16,25 @@ public class C03 {
 		
 		return n;
 	}
+	
+	public String solution(String str) {
+		String answer = "";
+		int m = Integer.MIN_VALUE, pos;
+		while((pos = str.indexOf(' '))!=-1) {
+			String tmp = str.substring(0, pos);
+			int len = tmp.length();
+			if(len > m) {
+				m = len;
+				answer = tmp;
+			}
+			str = str.substring(pos+1);
+		}
+		
+		if(str.length()>m) answer = str; // 마지막 단어
+		
+		return answer;
+	}
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
