185ceab08a3b084039655503bd2af0d8b4e09a51
Beginner <dkstmdwn17@naver.com>
2022-04-05 01:13:47 +0900

    중복문자제거
    중복문자제거

--- src/c1/C06.java
+++ src/c1/C06.java
@@ -20,6 +20,14 @@ public class C06 {
 		System.out.println(String.join("", set));
 	}
 	
+	public String solution(String str) {
+		String answer = "";
+		for(int i=0; i<str.length();i++) {
+			if(i==str.indexOf(str.charAt(i))) answer += str.charAt(i);
+		}
+		return answer;
+	}
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String str = sc.next();
