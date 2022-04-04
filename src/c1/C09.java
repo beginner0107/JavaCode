6688ed49365627b9c436fd519ca793614f734fe9
Beginner <dkstmdwn17@naver.com>
2022-04-05 01:14:33 +0900

    숫자만 추출
    아스키 코드값을 가지고 풀이

--- src/c1/C09.java
+++ src/c1/C09.java
@@ -10,6 +10,14 @@ public class C09 {
 		System.out.println(answer);
 	}
 	
+	public int solution(String s) {
+		int answer = 0;
+		for(char x : s.toCharArray()) {
+			if(x>=48 && x<=57) answer = answer * 10+(x-48);
+		}
+		return answer;
+	}
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String str = sc.next();
